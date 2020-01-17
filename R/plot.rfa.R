5c706e8f01bf9ac7c04d13fe4c93fb29d36669cb
Miles Williams <milesdwilliams15@gmail.com>
2020-01-17 13:26:57 -0600

    Add files via upload

---
+++ R/plot.rfa.R
@@ -0,0 +1,12 @@
+#' Wrapper for ggplot2 to Plot Histogram of Empirical ATE Replicates
+#'
+#' This function takes output from a `rfa` object and returns a
+#' histogram of the bootstrapped ATE replicates.
+#'
+#' @export
+plot.rfa = function(model){
+  ggplot2::qplot(model$bootate,
+                 xlab = "Empirical Distribution of ATE\n(Vertical Line Denotes Observed Estimate)",
+                 ylab = "Frequency\n(out of 999 replicates)") +
+    ggplot2::geom_vline(xintercept = model$ate)
+}
